412c62192700936b911158c2888a0ed26d7102ba
Sasidharan33 <dharansasi566@gmail.com>
2024-01-10 14:40:27 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Myform from './Form';
+import './style.css';
+function App() {
+  return (
+    <div className="App">
+      <Myform />
+    </div>
+  );
+}
+
+export default App;
